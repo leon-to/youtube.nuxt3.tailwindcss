df68778062759fd94dff9492542da35dc4377adb
Leon To <khoito100@gmail.com>
2022-04-05 11:59:10 -0400

    add flowbite sidenavbar

--- components/TopNavBar.vue
+++ components/TopNavBar.vue
@@ -2,7 +2,7 @@
   <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
       <a href="https://flowbite.com" class="flex items-center">
-        <img src="/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
       </a>
       <div class="flex items-center md:order-2">
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col h-screen">
     <top-nav-bar class="flex-0 w-screen h-16 bg-blue-500"/>
     <div class="flex flex-row w-full h-full">  
-      <side-nav-bar class="flex-0 w-40 bg-yellow-500 h-full" />
+      <side-nav-bar/>
       <div class="flex flex-col w-full h-full">
         <tag-banner class="h-10 bg-green-300"/>
         <content-list class="flex-0 h-full w-full bg-red-400"/>
