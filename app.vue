37bbd4927d60c198b1f833d41ade8f0fdf7d6c11
Leon To <khoito100@gmail.com>
2022-04-07 16:55:58 -0400

    add layout binding

--- app.vue
+++ app.vue
@@ -1,4 +1,19 @@
 <template>
+
+  <!-- <NuxtPage /> -->
   <!-- <NuxtLayout name="layout" /> -->
-  <NuxtLayout name="component" />
+  <NuxtLayout v-bind:name="name" />
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      name: 'component'
+    }
+  },
+  setup() {
+    
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div />
+  <div>
+    <button @click="enableCustomLayout">Update layout</button>
+  </div>
 </template>
+
+<script setup>
+const route = useRoute()
+function enableCustomLayout () {
+  route.meta.layout = "layout"
+}
+definePageMeta({
+  layout: false,
+});
+</script>
